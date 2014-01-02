5e8ace4e914719e077255a3b2c3a5b5a344fccb2
adam salem <adam.salem@adp.com>
2014-01-01 22:27:08 -0500

    Initial commit

--- spec/login_grid__ie_spec.rb
+++ spec/login_grid__ie_spec.rb
@@ -1,20 +1,24 @@
 require 'rspec'
 require 'rspec-expectations'
 require 'selenium-webdriver'
+require 'win32ole'
 
   describe 'Autotheme' do
 
-  email1 = 'anwar'
-  email2 = '@'
-  #email3 = 'ADP'
-  email4 = '.com'
-
         before(:each) do
-          @driver = Selenium::WebDriver.for :firefox
-          @base_url = 'http://localhost:8080/'
-          @accept_next_alert = true
+
+          @driver = Selenium::WebDriver.for(:remote, :url => 'http://localhost:4444/wd/hub', :desired_capabilities => :internet_explorer)
+
+          @site_url = '192.168.0.113'
+          @base_url = "http://#{@site_url}:8080"
           @driver.manage.timeouts.implicit_wait = 30
+          @driver.get(@base_url)
+          sleep 5
+          #@driver.execute_script("window.resizeTo(screen.width,screen.height)")
+          #@driver.execute_script("alert('Hello World');")
+          @driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
           @verification_errors = []
+          @accept_next_alert = true
           @screen_shot = rand(1..999999999999)
           puts @screen_shot
           @email1 = 'anwar'
@@ -30,18 +34,20 @@ require 'selenium-webdriver'
         end
 
         it 'Login with valid user and password' do
-          @driver.get(@base_url + '/')
+          @driver.get(@base_url)
+          @driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+          @driver.execute_script("alert('Hello World');")
+          sleep 3
           @driver.find_element(:id, 'email').clear
           @driver.find_element(:id, 'email').send_keys ("#{@email}")
           @driver.find_element(:xpath, '/html/body/div/form/input[2]').clear
           @driver.find_element(:xpath, '/html/body/div/form/input[2]').send_keys 'xxxxxx'
           @driver.find_element(:id, 'loginButton').click
+          @driver.find_element(:)
           @driver.find_element(:xpath, '/html/body/section[1]/nav/ul/li[1]/a/span').displayed?.should be_true
-          @time = Time.now.to_s
-          @tail = "#{@time}"
-          @driver.save_screenshot ("navigation/screenshot/#{@screen_shot}.png")
+          @driver.save_screenshot ("screenshot/#{@screen_shot}.png")
           @driver.title.should == 'App'
-          @driver.current_url.should == "http://localhost:8080/#{@email3}#/page:Home"
+          @driver.current_url.should == "http://#{@site_url}:8080/#{@email3}#/page:Home"
           @driver.current_url.should match(/#{@email3}/)
           @driver.current_url.should include("#{@email3}")
           @driver.current_url.should include("#/page:Home")
@@ -52,4 +58,4 @@ require 'selenium-webdriver'
 
         end
 
-end
+  end
--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -4,15 +4,17 @@ require 'selenium-webdriver'
 
   describe 'Autotheme' do
 
+  email1 = 'anwar'
+  email2 = '@'
+  #email3 = 'ADP'
+  email4 = '.com'
+
         before(:each) do
-
-          @driver = Selenium::WebDriver.for(:remote, :url => 'http://localhost:4444/wd/hub', :desired_capabilities => :firefox)
-
-          @site_url = '192.168.0.113'
-          @base_url = "http://#{@site_url}:8080"
+          @driver = Selenium::WebDriver.for :firefox
+          @base_url = 'http://localhost:8080/'
+          @accept_next_alert = true
           @driver.manage.timeouts.implicit_wait = 30
           @verification_errors = []
-          @accept_next_alert = true
           @screen_shot = rand(1..999999999999)
           puts @screen_shot
           @email1 = 'anwar'
@@ -29,19 +31,17 @@ require 'selenium-webdriver'
 
         it 'Login with valid user and password' do
           @driver.get(@base_url + '/')
-          sleep 2
           @driver.find_element(:id, 'email').clear
           @driver.find_element(:id, 'email').send_keys ("#{@email}")
           @driver.find_element(:xpath, '/html/body/div/form/input[2]').clear
           @driver.find_element(:xpath, '/html/body/div/form/input[2]').send_keys 'xxxxxx'
           @driver.find_element(:id, 'loginButton').click
-          sleep 2
           @driver.find_element(:xpath, '/html/body/section[1]/nav/ul/li[1]/a/span').displayed?.should be_true
           @time = Time.now.to_s
           @tail = "#{@time}"
-          @driver.save_screenshot ("screenshot/#{@screen_shot}.png")
+          @driver.save_screenshot ("spec/screenshot/#{@screen_shot}.png")
           @driver.title.should == 'App'
-          @driver.current_url.should == "http://#{@site_url}:8080/#{@email3}#/page:Home"
+          @driver.current_url.should == "http://localhost:8080/#{@email3}#/page:Home"
           @driver.current_url.should match(/#{@email3}/)
           @driver.current_url.should include("#{@email3}")
           @driver.current_url.should include("#/page:Home")
